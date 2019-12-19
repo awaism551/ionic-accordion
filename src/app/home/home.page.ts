d6ee0c85e5e7764e868679c64b50303c20cf05dc
Awais Nasir <awaism551@gmail.com>
2019-12-19 15:27:29 +0500

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,44 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+
+/**
+ * The data structure that will be used for supplying the accordion content
+ * @public
+ * @property technologies
+ * @type {Array}
+ */
+public technologies : Array<{ name: string, description: string, image: string }> = [
+    {
+       name : 'Angular',
+       description : 'Google\'s front-end development framework - default option for Ionic development',
+       image: '/assets/images/angular-logo.png'
+    },
+    {
+       name : 'VueJS',
+       description : 'Latest cutting edge front-end development framework - can be enabled as an option for Ionic development',
+       image: '/assets/images/vuejs-logo.png'
+    },
+    {
+       name : 'React',
+       description : 'Popular front-end development framework from Facebook- can be enabled as an option for Ionic development',
+       image: 'assets/images/react-logo.png'
+    }
+];
+
+	constructor() {}
+
+
+	/**
+	 * Captures and console logs the value emitted from the user depressing the accordion component's <ion-button> element
+	 * @public
+	 * @method captureName
+	 * @param {any}		event 				The captured event
+	 * @returns {none}
+	 */
+	public captureName(event: any) : void
+	{
+		console.log(`Captured name by event value: ${event}`);
+	}
 
 }
